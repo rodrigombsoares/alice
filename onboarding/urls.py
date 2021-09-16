5ec5d6c94095523245057c322bc419bb202369fa
renanrodriguesmr <renan.rodriguesmr@gmail.com>
2021-09-15 21:48:26 -0300

    Add models to onboarding app reflecting form

--- onboarding/urls.py
+++ onboarding/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 
 from django.urls import path
-from . import views
+from .views import OnboardingView
 
 urlpatterns = [
-    path('', views.index),
+    path('', OnboardingView.as_view()),
 ]
--- onboarding/views.py
+++ onboarding/views.py
@@ -1,8 +1,24 @@
+from django.views import View
 from django.shortcuts import render
-from django.template import loader
+from .models import Target, Manager
 
-# Create your views here.
+class OnboardingView(View):
+    def get(self, request):
+        return render(request, 'index.html')
 
+    def post(self, request):
+        name = request.POST.get('name')
+        phone_number = request.POST.get('phone_number')
+        email = request.POST.get('email')
+        targets = request.POST.get('targets')
 
-def index(request):
-    return render(request, 'index.html')
+        manager = Manager(name=name, phone_number=phone_number, email=email)
+        manager.save()
+
+        targetEmails = targets.split(",")
+        for targetEmail in targetEmails:
+            target = Target(email=targetEmail)
+            target.save()
+            manager.targets.add(target)
+
+        return render(request, 'index.html')
